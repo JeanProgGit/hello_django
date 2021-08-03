40f62939653ecf5c4506ca632b88625d76aa57f6
JeanProgGit <54381415+JeanProgGit@users.noreply.github.com>
2021-08-03 10:44:35 -0300

    Novas rotas adicionadas. Sub,Mult,Div

--- core/views.py
+++ core/views.py
@@ -4,3 +4,20 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse('<h1>Hello world {} de {} anos</h1>'.format(nome, idade))
+
+def soma(request, num1, num2):
+    soma = num1 + num2
+    return HttpResponse('<h1>{} + {} = {} </h1>'.format(num1, num2, soma))
+
+def sub(request, num1, num2):
+    sub = num1 - num2
+    return HttpResponse('<h1>{} - {} = {} </h1>'.format(num1, num2, sub))
+
+def mult(request, num1, num2):
+    mult = num1 * num2
+    return HttpResponse('<h1>{} * {} = {} </h1>'.format(num1, num2, mult))
+
+def div(request, num1, num2):
+    div = num1 / num2
+    return HttpResponse('<h1>{} / {} = {} </h1>'.format(num1, num2, div))
+
